50b445caba50a9c57fb9ab9419ee1da83e0609d4
Voriah <rodgick@yahoo.com>
2018-08-30 14:41:23 -0700

    times

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,18 +8,36 @@ var choiceThreeYear;
 var choiceFourYear;
 var time = 60;
 var stopTime;
+var highScore = 0;
 
 function countdown() {
+  wins = 0;
+  losses = 0;
+  time = 60;
+  $("#right").html(wins);
+  $("#rong").html(losses);
+  $("#result").html("");
+  $("#start").css("display", "none");
+  
   stopTime = setInterval(timer, 1000)
 }
 
 function timer () { 
-    $("#timer").text(time)
-    time--;
-    if (time < 0) {
-        clearInterval(stopTime);
-
-      }
+  $("#timer").text(time)
+  time--;
+  if (time < 0) {
+    clearInterval(stopTime);
+    hideQ();
+    $("#result").html(`
+    <center><p>You got ${wins} right!</p></center>
+    <center><p>Play again?</p></center>
+    `)
+    $("#start").css("display", "initial");
+    if (wins > highScore) {
+      highScore = wins;
+      $("#high").html(highScore);
+    }
+  }
   }
 
 function setAnswer() {
@@ -35,33 +53,68 @@ function setAnswer() {
 var range = Math.floor(Math.random() * 85 + 1930)
 slot = Math.floor(Math.random() * 4 +1);
 setTimeout(function() {
-var queryURL = "https://www.omdbapi.com/?t=the&y=" + range + "&apikey=trilogy";
-   $.get(queryURL)
-   .then(function(response) {
-     $("#hint").html(response.Plot)
-     $("#year").html(response.Year)
-     $(`#choice${slot}`).html(response.Title)
-     $(`#choice${slot}`).attr("data-value", 1)
-     console.log(response);
-     answerYear = response.Year;
-   }).catch(function (error) {
-     console.error(error);
-    });
-  }, 1000)
-  }
+  var queryURL = "https://www.omdbapi.com/?t=the&y=" + range + "&apikey=trilogy";
+  $.get(queryURL)
+  .then(function(response) {
+    $("#hint").html(response.Plot)
+    $("#year").html(response.Year)
+    $("#correctAnswer").html(response.Title)
+    $(`#choice${slot}`).html(response.Title)
+    $(`#choice${slot}`).attr("data-value", 1)
+    console.log(response);
+    answerYear = response.Year;
+  }).catch(function (error) {
+    console.error(error);
+  });
+}, 1000)
+}
 
-  function setChoices(){
-    setTimeout(function () {
-      setChoiceOne(); 
-      setChoiceTwo(); 
-      setChoiceThree();
-      setChoiceFour();
-    }, 1000);
-    setTimeout(function () {
-      $(".choiceses").css("visibility", "initial")
-      $("#stuff").css("visibility", "initial")
-    }, 3500);
-  }
+function results() {
+  
+}
+
+function correct() {
+  wins++
+  $("#right").html(wins)
+  setAnswer();
+  setChoices();
+  $("#correctAnswer").css("display", "initial")
+  $("#yes").css("display", "initial")
+  setTimeout(function() {
+    $("#correctAnswer").css("display", "none")
+    $("#yes").css("display", "none")
+  }, 1000)
+}
+
+function incorrect () {
+  
+  losses++
+  $("#rong").html(losses);
+  setAnswer();
+  setChoices();
+  $("#correctAnswer").css("display", "initial")
+  $("#no").css("display", "initial")
+  setTimeout(function() {
+    $("#correctAnswer").css("display", "none")
+    $("#no").css("display", "none")
+  }, 1000)
+  
+}
+function setChoices(){
+  setTimeout(function () {
+    setChoiceOne(); 
+    setChoiceTwo(); 
+    setChoiceThree();
+    setChoiceFour();
+  }, 1000);
+  setTimeout(function () {
+    if (time <= 0) {
+     return; 
+    }
+    $(".choiceses").css("visibility", "initial")
+    $("#stuff").css("visibility", "initial")
+  }, 3000);
+}
 
 function hideQ() {
   $(".choiceses").css("visibility", "hidden")
@@ -74,30 +127,19 @@ function guessAnswer1() {
   hideQ();
   console.log($("#choice1").text())
   if ($("#choice1").attr("data-value") === "1") {
-    wins++
-    $("#right").html(wins)
-    setAnswer();
-    setChoices();
+    correct();
   } else if ($("#choice1").attr("data-value") === "0"){
-    losses++
-    $("#rong").html(losses);
-    setAnswer();
-    setChoices();
+    incorrect();
   }
 }
 function guessAnswer2() {
   hideQ();
   console.log($("#choice2").attr("data-value"))
   if ($("#choice2").attr("data-value") === "1") {
-    wins++
-    $("#right").html(wins)
-    setAnswer();
-    setChoices();
+    correct();
+    
   } else if ($("#choice2").attr("data-value") === "0"){
-    losses++
-    $("#rong").html(losses);
-    setAnswer();
-    setChoices();
+    incorrect();
   }
   
 }
@@ -105,15 +147,10 @@ function guessAnswer3() {
   hideQ();
   console.log($("#choice3").attr("data-value"))
   if ($("#choice3").attr("data-value") === "1") {
-    wins++
-    $("#right").html(wins)
-    setAnswer();
-    setChoices();
+    correct();
+    
   } else if ($("#choice3").attr("data-value") === "0"){
-    losses++
-    $("#rong").html(losses);
-    setAnswer();
-    setChoices();
+    incorrect();
   }
   
 }
@@ -121,15 +158,10 @@ function guessAnswer4() {
   hideQ();
   console.log($("#choice4").attr("data-value"))
   if ($("#choice4").attr("data-value") === "1") {
-    wins++
-    $("#right").html(wins)
-    setAnswer();
-    setChoices();
+    correct();
+    
   } else if ($("#choice4").attr("data-value") === "0"){
-    losses++
-    $("#rong").html(losses);
-    setAnswer();
-    setChoices();
+    incorrect();
   }
   
 }
